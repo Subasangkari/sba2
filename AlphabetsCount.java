bb4468d8a75d44e6ddf214d6e8bf4413f4568aa1
Subasangkari <68595306+Subasangkari@users.noreply.github.com>
2020-08-06 17:07:51 +0530

    Add files via upload

---
+++ AlphabetsCount.java
@@ -0,0 +1,15 @@
+
+public class AlphabetsCount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		char ch='A';
+		for(i=0;i<=25;i++) {
+		System.out.print(ch + " " );
+		ch++;
+		}
+
+	}
+
+}
